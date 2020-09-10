64cf9af7f84c8020c29d0b99a76d6d5d2afbcda4
likithaaa <likithad1@gmail.com>
2020-09-10 14:45:32 -0400

    Redirect to homepage after registering

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { auth } from './firebase';
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -20,6 +21,9 @@ function Login() {
       .then((auth) => {
         //successfully created a new user with email and password
         console.log(auth);
+        if (auth) {
+          history.push('/');
+        }
       })
       .catch((error) => alert(error.message));
   };
