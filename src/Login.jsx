c33c398a885e12f0c387c4cb47e23c500a4dafec
likithaaa <likithad1@gmail.com>
2020-09-10 14:30:46 -0400

    Finish front-end of user-auth

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+    // firebase
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -16,12 +28,26 @@ function Login() {
         <h1>Sign In</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
         </form>
         <p>
           By signing-in you agree to the AMAZON CLONE Coditions of Use & Sale.
@@ -29,7 +55,7 @@ function Login() {
           Internet-Based Ads
         </p>
 
-        <button className="login__registerButton">
+        <button onClick={register} className="login__registerButton">
           Create your Amazon account
         </button>
       </div>
